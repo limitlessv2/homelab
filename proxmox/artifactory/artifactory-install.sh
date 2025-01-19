31f53735c7b0d05f99f15032d3609eda9adf3f81
limitlessv2 <pablorivas@utexas.edu>
2025-01-19 16:03:46 -0600

    Update artifactory-install.sh

--- proxmox/artifactory/artifactory-install.sh
+++ proxmox/artifactory/artifactory-install.sh
@@ -1,1 +1,19 @@
+#!/usr/bin/env bash
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: kristocopani
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+
+motd_ssh
+customize
+
+msg_ok "Cleaned"
